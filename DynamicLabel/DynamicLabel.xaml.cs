97e88016ce386d18324ef717ef73b056a32f826d
wehrenbrechtb <birger.wehrenbrecht@cryptshare.com>
2023-02-15 10:42:07 +0100

    Initial commit

---
+++ DynamicLabel/DynamicLabel.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DoubleVision
+{
+	public partial class DynamicLabel : UserControl
+	{
+		public static readonly DependencyProperty TextProperty =
+			DependencyProperty.Register("Text", typeof(string), typeof(DynamicLabel), new PropertyMetadata(string.Empty));
+
+		public DynamicLabel()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Enter text to be displayed.
+		/// </summary>
+		public object Text
+		{
+			get => (string)GetValue(TextProperty);
+			set => SetValue(TextProperty, value);
+		}
+
+		private void TextBlock_OnSizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			if (IsTextBlockTrimmed())
+				this.TextBlock.ToolTip = this.TextBlock.Text;
+		}
+
+		private bool IsTextBlockTrimmed()
+		{
+			var width = this.TextBlock.ActualWidth;
+			if (this.TextBlock.TextTrimming == TextTrimming.None) return false;
+			if (this.TextBlock.TextWrapping != TextWrapping.NoWrap) return false;
+			this.TextBlock.Measure(new Size(double.MaxValue, double.MaxValue));
+			var totalWidth = this.TextBlock.DesiredSize.Width;
+			return width < totalWidth;
+		}
+	}
+}
---
+++ WindowedPlayGround/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace WindowedPlayGround
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.DataContext = this;
+		}
+
+		public string PassedText { get; set; } = "Foo Bar Baaaaaaaaaaz";
+	}
+}
